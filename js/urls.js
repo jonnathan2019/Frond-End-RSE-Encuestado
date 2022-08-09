4b8b15963ec2d25d43da51df2384fbfa5e3a4a7d
jonnathancuvi2000 <jonnathancuvi2000@gmail.com>
2022-08-08 20:19:36 -0500

    frond-end-rse-encuestado

---
+++ js/urls.js
@@ -0,0 +1,5 @@
+var url_global_pagina = 'https://r-s-e-evaluacion-ucuenca.herokuapp.com/html/';
+
+// var link_service = `https://heruko-postgress.herokuapp.com/ejemplo01/`
+var link_service = `https://back-end-postgres-rse.herokuapp.com/`
+var extencion = `.php`
